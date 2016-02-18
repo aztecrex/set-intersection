bc35b42faa2297659178a6699b87252d3a0fe0e1
Greg Wiley <gwiley@cj.com>
2016-02-18 14:55:28 -0800

    pretty-up the property defs

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,13 +9,13 @@ main = do
   quickCheck propRightIdentity
 
 propAssoc :: Intersect -> Intersect -> Intersect -> Bool
-propAssoc xs ys zs = mappend xs (mappend ys zs) == mappend (mappend xs ys) zs
+propAssoc a b c =        a ⋅ (b ⋅ c) ≡ (a ⋅ b) ⋅ c
 
 propLeftIdentity :: Intersect -> Bool
-propLeftIdentity xs = mappend mempty xs == xs
+propLeftIdentity a =     φ ⋅ a ≡ a
 
 propRightIdentity :: Intersect -> Bool
-propRightIdentity xs = mappend xs mempty == xs
+propRightIdentity a =    a ⋅ φ ≡ a
 
 {--
 Newtype ensures we can create an instance of Monoid even though
@@ -38,7 +38,17 @@ instance Monoid Intersect where
   mempty = makeSet ['a'..'z']  -- the entire set of lower case letters
   mappend (Intersect xs) (Intersect ys) = Intersect $ intersection xs ys
 
+-- because we can...
+(⋅) :: (Monoid a) => a -> a -> a
+infixl 6 ⋅
+(⋅) = mappend
 
+φ :: (Monoid a) => a
+φ = mempty
+
+(≡) :: (Eq a) => a -> a -> Bool
+infix 4 ≡
+(≡) = (==)
 
 {--
 This is for generating arbitary subsets of the set of all lowercase
