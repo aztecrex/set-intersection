bb593e51654032265471f2151045a5151b8b57de
Greg Wiley <gwiley@cj.com>
2016-02-18 10:09:20 -0800

    working

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,31 @@
+import Test.QuickCheck
+import Data.Set (Set (..), empty, insert, intersection)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  putStrLn ""
+  quickCheck propAssoc
+  quickCheck propLeftIdentity
+  quickCheck propRightIdentity
+
+propAssoc :: String -> String -> String -> Bool
+propAssoc xs ys zs = mappend x (mappend y z) == mappend (mappend x y) z
+  where x = makeSet xs
+        y = makeSet ys
+        z = makeSet zs
+
+propLeftIdentity :: String -> Bool
+propLeftIdentity xs = mappend mempty xs == xs
+
+propRightIdentity :: String -> Bool
+propRightIdentity xs = mappend xs mempty == xs
+
+newtype Intersect a = Intersect (Set a) deriving (Show, Eq)
+
+makeSet :: (Ord a) => [a] -> Intersect a
+makeSet xs = Intersect ys
+  where ys = foldr insert empty xs
+
+instance (Ord a) => Monoid (Intersect a) where
+  mempty = Intersect empty
+  mappend (Intersect xs) (Intersect ys) = Intersect $ intersection xs ys
